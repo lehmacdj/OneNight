b1dae1a9d1420da0fb4ce3bea1008f012a1bb9f6
devin <devin@mango1>
2015-08-10 10:38:27 -0400

    Attempts to send a message to the main user

--- src/main/OneNightProtocol.java
+++ src/main/OneNightProtocol.java
@@ -5,8 +5,19 @@ package main;
  */
 public class OneNightProtocol {
     
-    public static String processInput(String input) {
+    static enum State {
         
+    }
+    
+    static State state;
+    
+    public static String processInput(String input) {
+        if (input == null)
+            return "Everyone has connected to the one night server let the games begin!  Please enter your name:  ";
+        switch (input) {
+            case "":
+                
+        }
         return null;
     }
 }
