42799aa096cce612146c4222cd0ff9389c64ec4e
Devin Lehmacher <lehmacdj@gmail.com>
2015-08-11 10:27:52 -0400

    Updated to use UserHandler to deal with multiple users

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,43 +14,9 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server(PORT, 3);
         
-        ArrayList<BufferedReader> in = new ArrayList<>();
-        ArrayList<PrintStream> out = new ArrayList<>();
-        
-        try {
-            //open the in and out streams
-            for (Socket s : server.getConnections()) {
-               out.add(new PrintStream(s.getOutputStream(), false));
-               in.add(new BufferedReader(new InputStreamReader(s.getInputStream())));
-            }
-            String input = null;
-            boolean inProgress = true;
-            while (inProgress) {
-                for (PrintStream o : out) {
-                    o.print(OneNightProtocol.processInput(input));
-                    o.flush();
-                }
-                for (BufferedReader i : in) {
-                    input = i.readLine();
-                }
-            }
-            
-            
-            
-        } catch (IOException e) {
-            System.err.println("Uh-oh there was an IO Exception.");
-        } finally {
-            //close in and out streams
-            try {
-                for (BufferedReader i : in) {
-                    i.close();
-                }
-                for (PrintStream o : out) {
-                    o.close();
-                }
-            } catch (IOException e) {
-                System.err.println("There was an IO Exception while closing in and out streams.");
-            }
+        for (Socket s : server.getConnections()) {
+            new Thread(new UserHandler(s)).start();
         }
+   
     }
 }
