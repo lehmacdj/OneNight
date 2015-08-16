ea3c7a08dd3c782ba375374e2aae5a8ce4a864e4
Devin Lehmacher <lehmacdj@gmail.com>
2015-08-15 20:34:18 -0400

    Added some comments about next steps to do

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,9 +11,13 @@ public class Main {
     
     static final int PORT = 9100;
     
-    public static void main(String[] args) {
-        Server server = new Server(PORT, 3);
+    public static void main(String[] args) { //pass roles via command line parameter
         
+        //parse command line parameters and use them to initialize a Roles array
+        
+        Server server = new Server(PORT, 3); //pass the Roles array to the server and assign each player a role during initialization
+        
+        //have the protocol use the roles assigned to players to personalize the protocol
         server.getConnections().stream().forEach( (s) -> {new Thread(new UserHandler(s)).start();} );
    
     }
