13fab9d94c7ddbe01f16f22aed73bb65a69200f4
Devin Lehmacher <lehmacdj@gmail.com>
2015-08-16 16:04:45 -0400

    Created registry of all roles

---
+++ src/roles/Registry.java
@@ -0,0 +1,44 @@
+package roles;
+
+/**
+ * @author Devin Lehmacher <lehmacdj@gmail.com>
+ */
+public class Registry {
+    
+    //A singleton structure
+    private static Registry instance = null;
+    public Registry getInstance() {
+        if (instance ==null) {
+            return instance = new Registry();
+        } else {
+            return instance;
+        }
+    }
+    
+    Role alphaWolf = new AlphaWolf();
+    Role apprenticeSeer = new ApprenticeSeer();
+    Role bodyguard = new Bodyguard();
+    Role curator = new Curator();
+    Role cursed = new Cursed();
+    Role doppelganger = new Doppelganger();
+    Role dreamWolf = new DreamWolf();
+    Role drunk = new Drunk();
+    Role hunter = new Hunter();
+    Role insomniac = new Insomniac();
+    Role mason = new Mason();
+    Role minion = new Minion();
+    Role mysticWolf = new MysticWolf();
+    Role paranormalInvestigator = new ParanormalInvestigator();
+    Role prince = new Prince();
+    Role revealer = new Revealer();
+    Role robber = new Robber();
+    Role seer = new Seer();
+    Role sentinel = new Sentinel();
+    Role tanner = new Tanner();
+    Role troublemaker = new Troublemaker();
+    Role villageIdiot = new VillageIdiot();
+    Role villager = new Villager();
+    Role werewolf = new Werewolf();
+    Role witch = new Witch();
+    
+}
