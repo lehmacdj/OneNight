037c3a307a6d868e973f49c1655c63b537c47d56
Devin Lehmacher <lehmacdj@gmail.com>
2015-08-16 21:40:01 -0400

    Added name and branch properties to player

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.net.*;
+import roles.Role;
 
 /**
  * @author devin
@@ -8,7 +9,25 @@ import java.net.*;
 public class Player {
     public final Socket SOCKET;
     
+    private Role role;
+    private String name;
+    
     public Player(Socket socket) {
         SOCKET = socket;
+        role = null;
+    }
+    
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
