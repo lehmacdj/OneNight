f6a9779a6d7a92ae97b865c837eb0be7205d6d30
Devin Lehmacher <lehmacdj@gmail.com>
2015-09-03 12:45:56 -0400

    Added javadoc.  Removed getSocket()

--- src/main/Player.java
+++ src/main/Player.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 import roles.Role;
 
 /**
+ * An implementation of CardLocation that represents a Player and their card.
  * @author devin
  */
 public class Player implements CardLocation {
@@ -18,10 +19,22 @@ public class Player implements CardLocation {
     private Role role;
     private String name;
     private final UUID uuid;
+    
+    /**
+     * A PrintWriter that prints to the client.  Provided for convenience.
+     */
     public final PrintWriter out;
+    
+    /**
+     * A buffered reader that reads from the client.
+     */
     public final BufferedReader in;
     
-    
+    /**
+     * Constructs a new instance of Player.
+     * @param socket The socket that is used to communicate with the client.
+     * @throws IOException when the provided socket cannot be used to get a in and out stream.
+     */
     public Player(Socket socket) throws IOException {
         this.socket = socket;
         role = null;
@@ -38,18 +51,28 @@ public class Player implements CardLocation {
         this.role = role;
     }
     
+    /**
+     * A method that provides the name of the player.
+     * @return the name of the Player object.
+     */
     public String getName() {
         return name;
     }
+    
+    /**
+     * Sets the player's name to name.
+     * @param name The name that the player's name should be set to.
+     */
     public void setName(String name) {
         this.name = name;
     }
     
+    /**
+     * Returns the UUID of the player.
+     * @return the UUID of the player.
+     */
     public UUID getUUID() {
         return uuid;
     }
     
-    public Socket getSocket() {
-        return socket;
-    }
 }
