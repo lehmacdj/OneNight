97986cb2f4adf19379c135ff599e13cc310b4ae6
Devin Lehmacher <lehmacdj@gmail.com>
2015-09-03 12:31:11 -0400

    Added javadoc

--- src/main/CardLocation.java
+++ src/main/CardLocation.java
@@ -3,9 +3,21 @@ package main;
 import roles.Role;
 
 /**
- * @author Devin Lehmacher <lehmacdj@gmail.com>
+ * An interface for things that store a the location of a card.
+ * @author devin
  */
 public interface CardLocation {
-    Role getRole();
-    void setRole(Role role);
+    
+	/**
+     * A method that should return the Role at the Location of this card.
+     * @return a role corresponding to the current location of this card.
+     */
+	Role getRole();
+    
+	/**
+	 * Sets the role of this object to role.
+	 * @param role The role that should be stored in this object.
+	 */
+	void setRole(Role role);
+    
 }
