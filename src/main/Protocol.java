d986a143df041bc362592f544a4905666d1fc0f9
Devin Lehmacher <lehmacdj@gmail.com>
2015-09-03 12:35:30 -0400

    Added javadoc

--- src/main/Protocol.java
+++ src/main/Protocol.java
@@ -1,5 +1,16 @@
 package main;
 
+/**
+ * A protocol to be used for the processing of user input.
+ * @author devin
+ */
 public interface Protocol {
+	
+	/**
+	 * Returns a string that is a response from the server to the client.
+	 * @param s The user input.
+	 * @return the String to be sent back to the client.
+	 */
 	String processInput(String s);
+
 }
